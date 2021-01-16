b475bb37c2d4a457974ca7ccdd2447f192edc853
hly1013 <hly1013@naver.com>
2021-01-16 15:28:46 +0900

    youtubemain backup

--- pages/mainPages/YoutubeMain.js
+++ pages/mainPages/YoutubeMain.js
@@ -1,70 +1,28 @@
-
 // TODO: look up to "SectionList" component
-//////////////////////////////////////////////////////////////////////////
-import React, { useEffect } from "react";
-import {
-    View,
-    StyleSheet,
-    FlatList,
-    Dimensions,
-    Text,
-    Image,
-    SectionList,
-} from "react-native";
+
+import React from "react";
+import { View, StyleSheet, FlatList, Dimensions, Text, Image } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { color } from "react-native-reanimated";
-//////////////////////////////////////////////////////////////////////////
 
-import Color from "../../constants/Color";
+import ListPhoto from "../../components/ListPhoto"
+
+import Color from "../../constants/Color"
 import CommonStyles from "../../constants/CommonStyle";
-
-import MainNavOptions from "../../components/MainNavOptions";
-
-//import SubHeader from "../../components/SubHeader";
-//import VideoList from "../../components/VideoList";
-//////////////////////////////////////////////////////////////////////////
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-const SECTIONS = [
-    {
-        smallTxt: "우리 주변 숨겨진",
-        hashTxt: "지역맛집",
-        data: imageDatas,
-    },
-    {
-        smallTxt: "매운 음식이 땡길때",
-        hashTxt: "매운음식 맛집",
-        data: imageDatas,
-    },
-    {
-        smallTxt: "우리 주변 숨겨진",
-        hashTxt: "지역맛집",
-        data: imageDatas,
-    },
-];
-
 const imageDatas = [
-    { name: "커리", length: "1:10", photo: require("../../assets/images/curry.jpg") },
-    { name: "포", length: "1:20", photo: require("../../assets/images/pho.jpeg") },
-    { name: "치킨", length: "1:50", photo: require("../../assets/images/chicken.jpg") },
+    { name: "커리", photo: require("../../constants/curry.jpg") },
+    { name: "포", photo: require("../../constants/pho.jpeg") },
+    { name: "치킨", hoto: require("../../constants/chicken.jpg") },
 ];
 
 const SubHeader = (props) => {
     return (
-        <View style={{width: windowWidth, height: windowHeight/11, alignItems: "flex-start", paddingLeft: 20, justifyContent: "center", backgroundColor: "white",
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.5,
-        shadowRadius: 3.3,
-
-        elevation: 5,
-        }}>
-            <Text style={{fontFamily: "noto_bold", fontSize: 17}}>{props.smallTxt}</Text>
+        <View style={{width: windowWidth, height: windowHeight/12, alignItems: "flex-start", paddingLeft: 20, justifyContent: "center", backgroundColor: "white"}}>
+            <Text style={{fontFamily: "noto_regular", fontSize: 17}}>{props.smallTxt}</Text>
             <Text style={{fontFamily: "noto_bold", fontSize: 24, color: "red"}}>#{props.hashTxt}</Text>
         </View>
     );
@@ -76,11 +34,9 @@ const VideoList = (props) => {
             numColumns={3}
             data={imageDatas}
             renderItem={({ item }) => {
-                return <ListPhoto ITEM_WIDTH={windowWidth / 2.3} item={item.photo} navigation={props.navigation} rest_name={item.name} video_length={item.length}/>;
+                return <ListPhoto ITEM_WIDTH={windowWidth / 2.3} item={item.photo} navigation={props.navigation} rest_name={item.name}/>;
             }}
             keyExtractor={(item, index) => index.toString()}
-            flexDirection= 'column'
-            scrollEnabled={true}
         ></FlatList>
     );
 }
@@ -98,7 +54,7 @@ const SubSection = (props) => {
 const YoutubeMain = (props) => {
     props.navigation.setOptions({
         headerBackTitleVisible: false,
-        headerTitle: (props) => <Image style={{ ...styles.logo, width: 130, height: 130 }} source={require("../../constants/logo_white.png")} resizeMode="contain"></Image>,
+        headerTitle: (props) => <Image style={{ width: 130, height: 130 }} source={require("../../constants/logo_white.png")} resizeMode="contain"></Image>,
         headerTitleStyle: {
             flex: 1,
             textAlign: "center",
@@ -106,55 +62,24 @@ const YoutubeMain = (props) => {
         headerStyle: {
             backgroundColor: Color.yellow,
             //width: windowWidth,
-            height: windowWidth * (5/17),
+            height: windowWidth * (5/16),
             
-            shadowOpacity: 0.5,
-            shadowRadius: 4
+            // 밑에 줄 그인 거 없애기 위함
+            shadowColor: "transparent",
         },
         //headerRight: () => <ProfileLogo touchable={true} navigation={props.navigation} style={{ marginRight: pad*1.2 }}></ProfileLogo>,
     });
 
     return (
-        <SectionList
-            sections={SECTIONS}
-            renderSectionHeader={({ section }) => (
-                <>
-                    <SubHeader
-                        smallTxt={section.smallTxt}
-                        hashTxt={section.hashTxt}
-                    ></SubHeader>
-                    <VideoList
-                        imageDatas={section.data}
-                        style={{}}
-                        navigation={props.navigation}
-                        width_divider={2.3}
-                    ></VideoList>
-                </>
-            )}
-            stickySectionHeadersEnabled={false}
-            renderItem={({ item, section }) => {
-                return null;
-            }}
-            showsHorizontalScrollIndicator={false}
-        ></SectionList>
+        <ScrollView>
+            <SubSection navigation={props.navigation} smallTxt={"우리 주변 숨겨진"} hashTxt={"지역맛집"} ></SubSection>
+            <SubSection navigation={props.navigation} smallTxt={"매운 음식이 땡길때"} hashTxt={"매운음식 맛집"}></SubSection>
+            <SubSection navigation={props.navigation} smallTxt={"우리 주변 숨겨진"} hashTxt={"지역맛집"}></SubSection>
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    logo: {
-        marginBottom: 18,
-        marginLeft: 25,
-
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.23,
-        shadowRadius: 3.84,
-
-        elevation: 5,
-    }
 });
 
 export default YoutubeMain;
